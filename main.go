8a43d34d1f7002851bfdb2d0b85106539cf140e7
Dmytro Maslov <Maslov.d.12@gmail.com>
2017-07-26 13:17:12 +0300

    DockerFile

--- main.go
+++ main.go
@@ -4,15 +4,22 @@ import (
 	"net/http"
 	"github.com/DmytroMaslov/testrepo/src/handlers"
 	"os"
+"fmt"
 )
 
 // ужас
 func main(){
+    fmt.Println("start")
+p:= os.Getenv("PORT")
+fmt.Println(p)
+    if p == ""{
+p = "8080"}
+    fmt.Println(p)
 	http.Handle("/client/", http.StripPrefix("/client/", http.FileServer(http.Dir("./client/"))))
 	http.HandleFunc("/", handlers.IndexHandler)
 	http.HandleFunc("/task/", handlers.HandlerTask)
 	http.HandleFunc("/tasks", handlers.HandlerTasks)
 
-	http.ListenAndServe(":"+os.Getenv("PORT"), nil)
+	http.ListenAndServe(":"+p, nil)
 }
 
